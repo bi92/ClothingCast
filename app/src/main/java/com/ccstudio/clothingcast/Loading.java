149a53f8dba91efad2c86cc3f715cbfc59feb717
Nayeon Kim <iamkite0209@gmail.com>
2020-08-10 03:21:21 +0900

    Splash Screen

---
+++ app/src/main/java/com/ccstudio/clothingcast/Loading.java
@@ -0,0 +1,26 @@
+package com.ccstudio.clothingcast;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 3000);
+    }
+}
--- app/src/main/java/com/ccstudio/clothingcast/MainActivity.java
+++ app/src/main/java/com/ccstudio/clothingcast/MainActivity.java
@@ -2,6 +2,7 @@ package com.ccstudio.clothingcast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 //https://github.com/firebase/snippets-android/blob/2228558fde6e576636e5ceadfe00ce0b9bdc0cdd/database/app/src/main/java/com/google/firebase/referencecode/database/MainActivity.java#L40-L44
@@ -17,12 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("mac test");
+        Intent intent = new Intent(this, Loading.class);
+        startActivity(intent);
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//
+//        myRef.setValue("mac test");
     }
 }
-
-//test for git repo
