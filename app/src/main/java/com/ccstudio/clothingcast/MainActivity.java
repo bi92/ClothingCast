e4ac4767e9356261065f9247d27bfa386c26a597
bi92 <tere4void@gmail.com>
2020-08-17 11:13:40 +0900

    1.parse k-weather json data 2. create list view of weatherDetail page 3. create Home page

--- app/src/main/java/com/ccstudio/clothingcast/MainActivity.java
+++ app/src/main/java/com/ccstudio/clothingcast/MainActivity.java
@@ -1,28 +1,38 @@
 package com.ccstudio.clothingcast;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
 //https://github.com/firebase/snippets-android/blob/2228558fde6e576636e5ceadfe00ce0b9bdc0cdd/database/app/src/main/java/com/google/firebase/referencecode/database/MainActivity.java#L40-L44
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+private Button btn_home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        btn_home = findViewById(R.id.movetoHome);
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        myRef.setValue("mac test");
+                //Home 화면 이동
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent); // 액티비티 이동
+            }
+        });
+
+
     }
+
+
 }
+
 
 //test for git repo
